07f68c49e5cb1ad079895eaea01d326c13f37af4
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-06-25 07:36:09 -0400

    Create state and local variables.sol

---
+++ state and local variables.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+contract A {
+constructor() public {
+uint new_StateVariable = 3;
+                    }
+
+function getResult public view returns(int memory){
+uint a = 3;
+uint b = 4;
+uint result= a+b;
+return result;
+}
+}
