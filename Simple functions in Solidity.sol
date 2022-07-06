66d9cee694e9441489a1a54d3573106ceed9fcc6
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-07-06 08:37:48 -0400

    Update Simple functions in Solidity.sol

--- Simple functions in Solidity.sol
+++ Simple functions in Solidity.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.7;
 contract FuncIntro {
 function sum(uint x, uint y) external pure returns (uint){
-return x+y;
+return x+y; //takes input from user
 }    
 
 function diff(uint x, uint y) external pure returns (uint){
@@ -13,7 +13,7 @@ function div(uint x, uint y) external pure returns (uint){
     return x/y;
 }
 function globVAr() external pure returns (uint) {
-    uint  y = 4;
+    uint  y = 4; //intializing local variable
     uint  x = 3;
     uint  z = x+y;
     return z;
