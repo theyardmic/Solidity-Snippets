d7746f4a244f93cd8b0bc0472b2d4f712af97f91
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-07-06 08:38:25 -0400

    Update Simple functions in Solidity.sol

--- Simple functions in Solidity.sol
+++ Simple functions in Solidity.sol
@@ -9,15 +9,16 @@ function diff(uint x, uint y) external pure returns (uint){
 return x-y;
 
 }
+
 function div(uint x, uint y) external pure returns (uint){
     return x/y;
 }
+
 function globVAr() external pure returns (uint) {
     uint  y = 4; //intializing local variable
     uint  x = 3;
     uint  z = x+y;
     return z;
 }
-
 
 }
