a83d8d2121894bd2984f9577b1e968a9db5479b4
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-07-10 07:58:14 -0400

    Update array operations.sol

--- array operations.sol
+++ array operations.sol
@@ -12,9 +12,17 @@ uint [10] public y = [1,2,3,4,5,6,7,8,9,10];
     x.pop();  //remove the last element
     x.length; //gets the length of the array
 } */
-function display() external view returns(uint){
+function length() external view returns(uint){
  uint z = x.length;
  return z;
+ }
+ function displaynewlength() external view returns(uint){
+ uint t = x.length;
+ return t;
+}
+function displayElement() external view returns(uint){
+ uint t = x[0];
+ return t;
 }
 function pop() external  {
 x.pop();    
@@ -23,10 +31,7 @@ x.pop();
 function push() external  {
 x.push(5);    
 } 
-function displaynewlength() external view returns(uint){
- uint t = x.length;
- return t;
-}
+
 }
 
 
