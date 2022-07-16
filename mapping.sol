e48fb61c4d950a46f1dc5e4cc252e617296493de
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-07-16 07:49:01 -0400

    Update mapping.sol

--- mapping.sol
+++ mapping.sol
@@ -19,7 +19,8 @@ contract ZombieFactory {
 
     function _createZombie(string memory _name, uint _dna) private {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
-        // start here
+         zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
     }
 
