c5d23e5b86ecc5ae2071f5ec7c14de0e463d66c9
TheScotlandyard <93291951+TheScotlandyard@users.noreply.github.com>
2022-09-29 03:14:07 -0400

    Update Mood.sol

--- Mood.sol
+++ Mood.sol
@@ -4,16 +4,13 @@ pragma solidity ^0.8.7;
 
 contract MoodDiary{
     
-    //create a variable called mood
-    string mood;
+     string mood;
     
-    //create a function that writes a mood to the smart contract
-    function setMood(string memory _mood) public{
+       function setMood(string memory _mood) public{
         mood = _mood;
     }
     
-    //create a function the reads the mood from the smart contract
-    function getMood() public view returns(string memory){
+      function getMood() public view returns(string memory){
         return mood;
     }
 }
