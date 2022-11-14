f23b811899384084d20ed3b2e22e968d596e3bde
Strucker.eth <aberamacarleton@gmail.com>
2022-11-15 00:37:48 +0300

    Simple Token Mint

--- tokenMint.sol
+++ tokenMint.sol
@@ -1,1 +1,13 @@
-SPDX-Lice
+//SPDX-License-Identifer: MIT 
+pragma solidity ^0.8.10;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract tokenMint{
+function mintToken (address _tokenAddress, address _to, uint256 _amount) public {
+    ERC20(_tokenAddress).mint(_to, _amount);
+}
+
+}
+
+
+
